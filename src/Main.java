f222f4e1134c12f20f0098b61a58e026b933334d
edmaker <edmaker@gmail.com>
2024-08-18 08:54:58 -0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.desafio.dominio.Bootcamp;
 import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Dev;
 import br.com.desafio.dominio.Mentoria;
 
 public class Main {
@@ -20,9 +22,32 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());;
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("BootCamp de Java - Claro");
+        bootcamp.setDescricao("Descrição bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev dev1 = new Dev();
+        dev1.setNome("Vanessa G. Nascimento");
+        dev1.inscreverBootCamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteudos concluidos: " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+        System.out.println("----------------------------------------------------");
+        
+        Dev dev2 = new Dev();
+        dev2.setNome("Douglsa E. Silva");
+        dev2.inscreverBootCamp(bootcamp);
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("Conteudos concluidos: " + dev2.getConteudosConcluidos());
+        System.out.println("Conteudos inscritos: " + dev2.getConteudosInscritos());
+        System.out.println("XP: " + dev2.calcularTotalXp());
+        
     }
 }
